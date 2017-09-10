ae09d7c24ff4f6f0f389198eacde1c0d40d79ef6
Elena <lenaburl@mail.ru>
2017-09-10 15:29:48 +0700

    Gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('less', function() {
+  gulp.src('src/less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest(''));
 });
