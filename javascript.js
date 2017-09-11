85a6f7426c1ed79138303f2afae0273b6147070a
Elena <lenaburl@mail.ru>
2017-09-11 16:01:46 +0700

    Change delays in js-file

--- javascript.js
+++ javascript.js
@@ -1,25 +1,23 @@
-
 $(function FixHeader(){
-  var h_mrg = 0;    // отступ когда шапка уже не видна
-var h_hght = $('#top-header').outerHeight(); //высота шапки
+  var h_mrg = 0;
+  var h_hght = $('#top-header').outerHeight();
 
-    var elem = $('#bottom-header');
-    var top = $(this).scrollTop();
+  var elem = $('#bottom-header');
+  var top = $(this).scrollTop();
 
-    if(top > h_hght){
-        elem.css('top', h_mrg);
+  if(top > h_hght){
+    elem.css('top', h_mrg);
+  }
+
+  $(window).scroll(function(){
+    top = $(this).scrollTop();
+
+    if (top+h_mrg < h_hght) {
+      elem.css('top', (h_hght-top));
+    } else {
+      elem.css('top', h_mrg);
     }
-
-    $(window).scroll(function(){
-        top = $(this).scrollTop();
-
-        if (top+h_mrg < h_hght) {
-            elem.css('top', (h_hght-top));
-        } else {
-            elem.css('top', h_mrg);
-        }
-    });
-
+  });
 });
 
 $(document).ready(function() {
